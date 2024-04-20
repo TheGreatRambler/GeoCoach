629a3a8511017727abdc896d57100a89656ccbd6
Tyler Wright <tyler.wright5146@gmail.com>
2024-04-20 07:19:10 -0700

    typo

--- backend/models.go
+++ backend/models.go
@@ -10,14 +10,14 @@ type App struct {
 
 type Round struct {
 	gorm.Model
-	RoundLat float64 `gorm:"type:decimal(10,8);not null"`
-	RoundLon float64 `gorm:"type:decimal(11,8);not null"`
-	GuessLat float64 `gorm:"type:decimal(10,8);not null"`
-	GuessLon float64 `gorm:"type:decimal(11,8);not null"`
-	Score    int     `gorm:"type:integer;not null"`
-	UserID   string  `gorm:"type:varchar(100);not null"`
-	Address string  `gorm:"type:varchar(1000);not null"`
-	PanoramaID string `gorm:"type:varchar(100);not null"`
+	RoundLat   float64 `gorm:"type:decimal(10,8);not null"`
+	RoundLon   float64 `gorm:"type:decimal(11,8);not null"`
+	GuessLat   float64 `gorm:"type:decimal(10,8);not null"`
+	GuessLon   float64 `gorm:"type:decimal(11,8);not null"`
+	Score      int     `gorm:"type:integer;not null"`
+	UserID     string  `gorm:"type:varchar(100);not null"`
+	Address    string  `gorm:"type:varchar(1000);not null"`
+	PanoramaID string  `gorm:"type:varchar(100);not null"`
 }
 
 type Tip struct {
@@ -25,4 +25,4 @@ type Tip struct {
 	RoundID   uint   `gorm:"type:integer;not null;foreignkey:RoundID"`
 	TipString string `gorm:"type:varchar(1000);not null"`
 	UserID    string `gorm:"type:varchar(100);not null"`
-}g
+}
